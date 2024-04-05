bdb5cacfd132d591fdd3b6266d4059f2c81f9150
Ralph Balazs <rbalazs@bess-f1r5s8.clusters.42paris.fr>
2024-04-05 22:14:11 +0200

    four

--- src/radix.c
+++ src/radix.c
@@ -48,13 +48,41 @@ void	sort_three_elements(t_stack *stack_a)
 		sa(stack_a, true);
 }
 
+/*
+void	sort_four_elements(t_stack	*stack_a)
+{
+	if (stack_a->tab[0] > stack_a->tab[1])
+		sa(stack_a, true);
+	rab(stack_a, 0);
+	if (!ft_issort(stack_a))
+		sort_four_elements(stack_a);
+	return ;
+}
+*/
+
+void sort_four_elements(t_stack *stack_a) {
+    if (ft_issort(stack_a)) // Vérifiez d'abord si le tableau est déjà trié
+        return;
+
+    if (stack_a->tab[0] > stack_a->tab[1]) // Assurez-vous que le plus petit élément est en haut
+        sa(stack_a, true);
+
+    rab(stack_a, 0); // Rotation à droite pour placer le plus petit élément en bas
+
+    if (!ft_issort(stack_a)) // Vérifiez à nouveau si le tableau est trié
+        sort_four_elements(&stack_a);
+}
+
 void	ft_radix(t_stack stack_a)
 {
 	t_stack	stack_b;
 
 	stack_b = init_stack_b(&stack_a);
 	stack_a = create_index(&stack_a);
-	sort_three_elements(&stack_a);
+	if	(stack_a.len == 3)
+		sort_three_elements(&stack_a);
+	if	(stack_a.len == 4)
+		sort_four_elements(&stack_a);
 	free(stack_b.tab);
 	printf("\033[0;33mAprès le radix :\033[0m\n");
 	print_tab(&stack_a);
