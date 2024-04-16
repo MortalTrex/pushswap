183919c28a1da8844768278e2e582b2e757c7c6a
Ralph Balazs <rbalazs@bess-f1r5s4.clusters.42paris.fr>
2024-04-16 21:53:12 +0200

    format

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,70 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbalazs <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: rbalazs <rbalazs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:36:52 by rbalazs           #+#    #+#             */
-/*   Updated: 2024/02/19 10:38:01 by rbalazs          ###   ########.fr       */
+/*   Updated: 2024/04/16 21:52:50 by rbalazs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "../libft/libft_core/libft.h"
 # include "../libft/ft_printf/ft_printf.h"
+# include "../libft/libft_core/libft.h"
 # include <stdbool.h>
 
-//STRUCTURES
+// STRUCTURES
 typedef struct s_stack
 {
-	int	*tab;
-	int	len;
-}			t_stack;
+	int					*tab;
+	int					len;
+}						t_stack;
 
 typedef struct s_stack_node
 {
-	int		nbr;
+	int					nbr;
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;
-}	t_stack_node;
+}						t_stack_node;
 
-//INSTRUCTIONS
-void	sa(t_stack *stack_a, bool print);
-void	sb(t_stack *stack_b, bool print);
-void	ss(t_stack *stack_a, t_stack *stack_b);
-void	pa(t_stack *stack_a, t_stack *stack_b);
-void	pb(t_stack *stack_a, t_stack *stack_b);
-void	rab(t_stack *stack_ab, int stack);
-void	rrab(t_stack *stack_a, int stack);
-void	rrr(t_stack *stack_a, t_stack *stack_b);
+// INSTRUCTIONS
+void					sa(t_stack *stack_a, bool print);
+void					sb(t_stack *stack_b, bool print);
+void					ss(t_stack *stack_a, t_stack *stack_b);
+void					pa(t_stack *stack_a, t_stack *stack_b);
+void					pb(t_stack *stack_a, t_stack *stack_b);
+void					rab(t_stack *stack_ab, int stack);
+void					rrab(t_stack *stack_a, int stack);
+void					rrr(t_stack *stack_a, t_stack *stack_b);
 
-//FREE
-void	free_stack(t_stack_node **stack);
-void	free_errors(t_stack_node **stack_a);
-//VERIF
+// FREE
+void					free_stack(t_stack_node **stack);
+void					free_errors(t_stack_node **stack_a);
 
-void	print_tab(t_stack	*stacktab);
-bool	ft_verifdouble(t_stack_node *stack_a, int numbers);
-bool	ft_verifsyntax(char *argv);
-bool	ft_issort(t_stack stacktab);
+// VERIF
+void					print_tab(t_stack *stacktab);
+bool					ft_verifdouble(t_stack_node *stack_a, int numbers);
+bool					ft_verifsyntax(char *argv);
+bool					ft_issort(t_stack stacktab);
 
-//PARSING
-void	init_stack_a(t_stack_node **stack_a, char **argv);
-t_stack	init_stack_b(void);
+// PARSING
+void					init_stack_a(t_stack_node **stack_a, char **argv);
+t_stack					init_stack_b(void);
 
-//UTILS
-t_stack_node	*find_last(t_stack_node *stack);
-int		ft_stack_len(t_stack_node *stack);
+// UTILS
+t_stack_node			*find_last(t_stack_node *stack);
+int						ft_stack_len(t_stack_node *stack);
 
-//CONVERSION
-t_stack	ft_hybrid_listotab(t_stack_node *stacklist);
+// CONVERSION
+t_stack					ft_hybrid_listotab(t_stack_node *stacklist);
 
-//RADIX
-void	ft_radix(t_stack stack_a);
+// RADIX
+void					ft_radix(t_stack stack_a);
 
-//PRINT
-void	print_stack(t_stack_node *stack);
-void	print_tab(t_stack	*stacktab);
+// PRINT
+void					print_stack(t_stack_node *stack);
+void					print_tab(t_stack *stacktab);
 
 #endif
